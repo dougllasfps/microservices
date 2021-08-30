214faf19bdeab989b078451471dfc224ca14376c
Dougllas Sousa <dougllasfps@gmail.com>
2021-08-30 10:25:46 -0300

    complete configuration

--- discovery/src/test/java/io/github/dougllasfps/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/io/github/dougllasfps/discovery/DiscoveryApplicationTests.java
@@ -1,13 +1,13 @@
-package io.github.dougllasfps.discovery;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DiscoveryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package io.github.dougllasfps.discovery;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DiscoveryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
