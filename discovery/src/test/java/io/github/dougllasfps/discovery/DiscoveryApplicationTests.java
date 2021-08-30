dac1128f3602011bf5e4a16011b8c9c50a00a4e6
Dougllas Sousa <dougllasfps@gmail.com>
2021-08-30 08:08:52 -0300

    add discovery project

---
+++ discovery/src/test/java/io/github/dougllasfps/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dougllasfps.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
